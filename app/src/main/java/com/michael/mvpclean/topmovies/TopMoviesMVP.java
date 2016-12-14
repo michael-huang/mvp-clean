93b5e53f27e90a9677eb5c2542c7e3ec1e7ea7a5
Michael Huang <hyc1124@gmail.com>
2016-12-14 20:56:14 +0800

    init MVP framework

---
+++ app/src/main/java/com/michael/mvpclean/topmovies/TopMoviesMVP.java
@@ -0,0 +1,33 @@
+package com.michael.mvpclean.topmovies;
+
+import rx.Observable;
+
+/**
+ * Created by huangyanzhen on 2016/12/14.
+ */
+
+public interface TopMoviesMVP {
+    interface View {
+
+        void updateData(ViewModel viewModel);
+
+        void showSnackbar(String s);
+
+    }
+
+    interface Presenter {
+
+        void loadData();
+
+        void rxUnsubscribe();
+
+        void setView(TopMoviesMVP.View view);
+
+    }
+
+    interface Model {
+
+        Observable<ViewModel> result();
+
+    }
+}
