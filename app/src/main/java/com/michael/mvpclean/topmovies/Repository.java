6cb5807f444d2e578fc818c29be0a06e8d6ad835
Michael Huang <hyc1124@gmail.com>
2016-12-14 21:17:15 +0800

    declare an interface of Repository

---
+++ app/src/main/java/com/michael/mvpclean/topmovies/Repository.java
@@ -0,0 +1,25 @@
+package com.michael.mvpclean.topmovies;
+
+import com.michael.mvpclean.api.model.Result;
+
+import rx.Observable;
+
+/**
+ * Created by huangyanzhen on 2016/12/14.
+ */
+
+public interface Repository {
+
+    Observable<Result> getResultsFromMemory();
+
+    Observable<Result> getResultsFromNetwork();
+
+    Observable<String> getCountriesFromMemory();
+
+    Observable<String> getCountriesFromNetwork();
+
+    Observable<String> getCountryData();
+
+    Observable<Result> getResultData();
+
+}
