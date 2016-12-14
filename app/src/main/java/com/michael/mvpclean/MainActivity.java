810926feff0b88c1be7584d751a024bdd4e1fd0c
Michael Huang <hyc1124@gmail.com>
2016-12-14 20:42:20 +0800

    binding view with butterknife

--- app/src/main/java/com/michael/mvpclean/MainActivity.java
+++ app/src/main/java/com/michael/mvpclean/MainActivity.java
@@ -1,13 +1,27 @@
 package com.michael.mvpclean;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @BindView(R.id.recycler_view)
+    RecyclerView recyclerView;
+
+    @BindView(R.id.ll_root_view)
+    LinearLayout rootView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
